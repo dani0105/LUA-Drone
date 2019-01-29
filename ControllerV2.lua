a979f7f1a3335b82c30034d2811a578fd8eb63b6
dani0105 <danielrojas0105@gmail.com>
2019-01-28 22:30:48 -0600

    Errores

--- ControllerV2.lua
+++ ControllerV2.lua
@@ -1,17 +1,5 @@
 rednet.open("back")
-rednet.broadcast("done")
-local id,data =rednet.receive()
-
-term.clear()
-term.setCursorPos(1,1)
-write("Fuel:"..data[5].."     Direccion:"..data[4])
-term.setCursorPos(1,3)
-print("Frente:"..data[1])
-print("Arriba:"..data[2])
-print("Abajo:"..data[3])
-
 local state = "ready"
-
 while true do 
 	if state == "ready" then
 		local event, key = os.pullEvent("key")
--- DroneV2.lua
+++ DroneV2.lua
@@ -1,30 +1,11 @@
 rednet.open("right")
-local state = "wating"
 print("Puerto Abierto, listo para la conexion")
 write("\nID de la computadora:")
 local idComputer= tonumber(read())
 
-print("esperando conexion")
-while state == "wating" do
-	local id,response =rednet.receive()
-	if id == idComputer and response == "done" then
-		state= "ready"
-	end
-end
 local directions = {"norte","este","sur","oeste"}
 local j = 1
 local direction = directions[j]
-local done,frente = turtle.inspect()
-if done==false then frente="nada" end
-local done,abajo = turtle.inspectUp()
-if done==false then abajo="nada" end
-local done,arriba = turtle.inspectDown()
-if done==false then arriba="nada" end
-fuel = turtle.getFuelLevel()
-
-Table = {frente,abajo,arriba,fuel,direction}
-rednet.send(idComputer,Table)
-
 
 while true do
 	local id,response =rednet.receive()
